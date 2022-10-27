cb7792d559ee8c0d3fdb22ac7dd92c6ce8b63af9
JoaorGuimaraes <joaoprogramacao123@gmail.com>
2022-10-27 12:01:16 -0300

    Dashboard done

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,18 +2,18 @@ import { Box, Icon, Link, Stack, Text } from "@chakra-ui/react";
 import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
 
 export function Sidebar() {
-    return(
+    return (
         <Box as="aside" width="64" mr="8">
             <Stack spacing="12" align="flex-start">
                 <Box>
                     <Text fontWeight="bold" color="gray.400" fontSize="small">GERAL</Text>
                     <Stack>
                         <Link display="flex" align="center">
-                            <Icon as={RiDashboardLine} fontSize="20"/>
+                            <Icon as={RiDashboardLine} fontSize="20" />
                             <Text ml="4" fontWeight="md">Dashboard</Text>
                         </Link>
                         <Link display="flex" align="center">
-                            <Icon as={RiContactsLine} fontSize="20"/>
+                            <Icon as={RiContactsLine} fontSize="20" />
                             <Text ml="4" fontWeight="md">Usuários</Text>
                         </Link>
                     </Stack>
@@ -23,11 +23,11 @@ export function Sidebar() {
                     <Text fontWeight="bold" color="gray.400" fontSize="small">AUTOMAÇÃO</Text>
                     <Stack>
                         <Link display="flex" align="center">
-                            <Icon as={RiInputMethodLine} fontSize="20"/>
+                            <Icon as={RiInputMethodLine} fontSize="20" />
                             <Text ml="4" fontWeight="md">Formulários</Text>
                         </Link>
                         <Link display="flex" align="center">
-                            <Icon as={RiGitMergeLine} fontSize="20"/>
+                            <Icon as={RiGitMergeLine} fontSize="20" />
                             <Text ml="4" fontWeight="md">Automação</Text>
                         </Link>
                     </Stack>
